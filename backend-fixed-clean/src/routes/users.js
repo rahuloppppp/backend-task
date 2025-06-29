69197829342a1945c9f7cc831a861d9abc84b578
rahuloppppp <rahulopppp223@gmail.com>
2025-06-29 22:39:39 +0530

    Update users.js

--- backend-fixed-clean/src/routes/users.js
+++ backend-fixed-clean/src/routes/users.js
@@ -1,19 +1,13 @@
-
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/auth");
-const {
-  follow,
-  unfollow,
-  getMyFollowers,
-  getMyFollowing,
-  getStats,
-} = require("../controllers/users");
 
-router.post("/:id/follow", authMiddleware, follow);
-router.delete("/:id/follow", authMiddleware, unfollow);
-router.get("/followers", authMiddleware, getMyFollowers);
-router.get("/following", authMiddleware, getMyFollowing);
-router.get("/:id/stats", authMiddleware, getStats);
+const usersController = require("../controllers/users");
+
+router.post("/:id/follow", authMiddleware, usersController.follow);
+router.delete("/:id/follow", authMiddleware, usersController.unfollow);
+router.get("/followers", authMiddleware, usersController.getMyFollowers);
+router.get("/following", authMiddleware, usersController.getMyFollowing);
+router.get("/:id/stats", authMiddleware, usersController.getStats);
 
 module.exports = router;
